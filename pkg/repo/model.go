5f284c082c44a6625e1ada0855feffb643dfdb6e
Bill Nunney <bill@bigtallbill.com>
2023-08-06 21:26:58 -0400

    Add comments

--- pkg/repo/model.go
+++ pkg/repo/model.go
@@ -1,5 +1,22 @@
 package repo
 
+// Model is a generic model.
+//
+// example:
+//
+//	type User struct {
+//		ID       primitive.ObjectID `bson:"_id"`
+//		Username string             `bson:"username"`
+//		Password string             `bson:"password"`
+//	}
+//
+//	func (u *User) GetDatabaseName() string {
+//		return "users_db"
+//	}
+//
+//	func (u *User) GetCollectionName() string {
+//		return "users_col"
+//	}
 type Model interface {
 	GetDatabaseName() string
 	GetCollectionName() string
