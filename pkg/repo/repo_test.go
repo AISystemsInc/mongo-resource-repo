f136a290c627625d44ab4fab1f70539734902da1
Bill Nunney <me@billnunney.dev>
2023-08-07 08:17:06 -0400

    Add new 'FindStream' test in repo_test.go
    - Imported "time" package.
    - Created a new struct FindStreamModel for testing purpose.
    - Added a new test function "TestRepository_FindStream" which includes the code for connecting MongoDB, inserting data, reading data from the database and comparing the read data with the inserted data.
    
    The new function essentially tests the functionality of the FindStream method in the repository. It tests the ability to find and retrieve specific streams of data from the MongoDB. The function also tests error handling and disconnection from the MongoDB. The test is conducted using mock data entered into the MongoDB and then retrieved using the FindStream method.

--- pkg/repo/repo_test.go
+++ pkg/repo/repo_test.go
@@ -9,6 +9,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type FindOneModel struct {
@@ -310,4 +311,108 @@ func TestRepository_Find(t *testing.T) {
 			}
 		})
 	}
+}
+
+type FindStreamModel struct {
+	ID primitive.ObjectID `bson:"_id"`
+}
+
+func (f *FindStreamModel) GetDatabaseName() string {
+	return "find_stream_model_db"
+}
+
+func (f *FindStreamModel) GetCollectionName() string {
+	return "find_stream_model_col"
+}
+
+func TestRepository_FindStream(t *testing.T) {
+	mongoClient, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:57018"))
+	if err != nil {
+		t.Errorf("error connecting to mongo: %v", err)
+		return
+	}
+
+	defer func() {
+		err := mongoClient.Disconnect(context.Background())
+		if err != nil {
+			t.Errorf("error disconnecting from mongo: %v", err)
+		}
+	}()
+
+	var insertDocuments = func(num int) ([]FindStreamModel, error) {
+		var models []FindStreamModel
+		for i := 0; i < num; i++ {
+			models = append(models, FindStreamModel{
+				ID: primitive.NewObjectID(),
+			})
+		}
+
+		var documents []interface{}
+		for _, model := range models {
+			documents = append(documents, model)
+		}
+
+		_, err := mongoClient.Database("find_stream_model_db").Collection("find_stream_model_col").InsertMany(context.Background(), documents)
+		return models, err
+	}
+
+	var tearDown = func() error {
+		return mongoClient.Database("find_stream_model_db").Collection("find_stream_model_col").Drop(context.Background())
+	}
+
+	t.Run("should return a stream of models", func(t *testing.T) {
+		inserted, err := insertDocuments(10)
+		if err != nil {
+			t.Errorf("error inserting documents: %v", err)
+			return
+		}
+
+		defer tearDown()
+
+		var repository = NewRepository[*FindStreamModel, primitive.ObjectID](mongoClient)
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		stream, errChan, cancelChan, err := repository.FindStream(ctx, bson.M{})
+		if err != nil {
+			t.Errorf("error creating stream: %v", err)
+			return
+		}
+
+		defer func() {
+			close(cancelChan)
+		}()
+
+		go func() {
+			for err := range errChan {
+				t.Errorf("error streaming: %v", err)
+				return
+			}
+		}()
+
+		var models []*FindStreamModel
+		for model := range stream {
+			models = append(models, model)
+		}
+
+		if len(models) != len(inserted) {
+			t.Errorf("expected %d models but got %d", len(inserted), len(models))
+			return
+		}
+
+		for _, model := range models {
+			var found bool
+			for _, insertedModel := range inserted {
+				if model.ID == insertedModel.ID {
+					found = true
+					break
+				}
+			}
+			if !found {
+				t.Errorf("expected model with id %s to be found", model.ID)
+				return
+			}
+		}
+	})
 }
